3ae7a519592de82ac86ea6e149856ed2ee97c562
Daniel Chiu <daniechiu@gmail.com>
2014-02-23 13:10:37 -0800

    Add server ticks and snapshots. Paddles will now move on opponent's screen, but entity interpolation has not been implemented yet.

--- public/js/constants.js
+++ public/js/constants.js
@@ -2,6 +2,7 @@ if (typeof exports == 'undefined') {
   var exports = this['constants'] = {}
 }
 
+// Game Constants
 exports.PADDLE_HEIGHT = 100;
 exports.PADDLE_WIDTH = 25;
 exports.PADDLE_START_Y = 250;
@@ -10,9 +11,21 @@ exports.DirectionEnum = Object.freeze({
   DOWN: "down",
   IDLE: "idle"
 });
+// How fast the paddles should move in pixels/millisecond
 exports.PADDLE_MOVE_SPEED = 600 / 1000;
+exports.GAME_HEIGHT = 600;
+exports.GAME_WIDTH = 800;
+
+/* Client Constants */
+// How long a move should take in milliseconds
 exports.MOVE_DURATION = 10;
 exports.FRAMES_PER_SECOND = 60;
 exports.FRAMES_PER_MOVE = Math.ceil(exports.MOVE_DURATION / exports.FRAMES_PER_SECOND);
 exports.MOVE_DISTANCE = exports.PADDLE_MOVE_SPEED * exports.MOVE_DURATION;
 exports.DISTANCE_PER_FRAME = exports.MOVE_DISTANCE / exports.FRAMES_PER_MOVE;
+
+/* Server Constants */
+// The tick rate of the server simulation steps in milliseconds
+exports.SERVER_TICK_RATE = 15;
+// How often the server sends snapshots to the clients in milliseconds
+exports.SERVER_SNAPSHOT_RATE = 100;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var send = require('send');
 var url = require('url');
+var _ = require('underscore');
+var constants = require('./public/js/constants');
 
 var app = http.createServer(function(req, res) {
 
@@ -22,69 +24,92 @@ var app = http.createServer(function(req, res) {
     .pipe(res);
 }).listen(3000);
 
-var io = require('socket.io').listen(app);
+var io = require('socket.io').listen(app, {log: false});
 
-var PADDLE_HEIGHT = 100;
-var GAME_HEIGHT = 600;
-var PADDLE_START_Y = 250;
 
-var paddleY = PADDLE_START_Y;
+var players = [];
+players[0] = {
+  connected: false
+};
+players[1] = {
+  connected: false
+};
 
-var playerOne = {};
-playerOne.connected = false;
-var playerTwo = {};
-playerTwo.connected = false;
+var actionQueue = [];
 
 io.sockets.on('connection', function(socket) {
   console.log("Connection established!");
-  if (playerOne.connected && playerTwo.connected) {
+  if (players[0].connected && players[1].connected) {
     console.log('More than two connections received');
     socket.disconnect();
     return;
   }
   else {
-    var player = playerOne.connected ? playerTwo : playerOne;
-    player.id = playerOne.connected ? 1 : 0;
+    var player = players[0].connected ? players[1] : players[0];
+    player.id = players[0].connected ? 1 : 0;
     player.connected = true;
     console.log("Assigned ID: " + player.id);
     socket.emit('assignID', player.id); 
     player.connection = socket;
-    if (playerOne.connected && playerTwo.connected) {
-      playerOne.connection.emit("ready");
-      playerTwo.connection.emit("ready");
+    if (players[0].connected && players[1].connected) {
       runGame();
     }
   }
 
   socket.on('disconnect', function() {
-    playerOne.connected = playerOne.socket != null;
-    playerTwo.connected = playerTwo.socket != null;
+    _.each(players, function(player) {
+      player.connected = player.socket != null;
+    });
   });
 
-  socket.on('keyPressed', function(data) {
-    console.log("Player pressed " + data + " key.");
-
-    if (data === "up") {
-      paddleY -= 10;
-    }
-    else if (data === "down") {
-      paddleY += 10;
-    }
-
-    checkBounds();
-    socket.emit('positionUpdate', paddleY);
+  socket.on('move', function(data) {
+    actionQueue.push(data);
   });
-
-  function checkBounds() {
-    if (paddleY + PADDLE_HEIGHT > GAME_HEIGHT) {
-      paddleY = GAME_HEIGHT - PADDLE_HEIGHT;
-    }
-    else if (paddleY < 0) {
-      paddleY = 0;
-    }
-  };
 });
 
+function initGame() {
+  _.each(players, function(player) {
+    player.connection.emit("ready");
+    player.position = constants.PADDLE_START_Y;
+  });
+}
+
+/**
+ * Computes the simulation for a single server tick by processing all actions
+ * received in the action queue and updating the server's internal model of the
+ * game.
+ */
+function computeServerTick() {
+  _.each(actionQueue, function(action) {
+    var player = action.id == 0 ? players[0] : players[1];
+    if (player.position != action.position) {
+      console.log("WARNING: Client state is inconsistent with server state.");
+    }
+    if (action.direction == constants.DirectionEnum.UP) {
+      player.position -= constants.MOVE_DISTANCE;
+    }
+    else if (action.direction == constants.DirectionEnum.DOWN) {
+      player.position += constants.MOVE_DISTANCE;
+    }
+
+    if (player.position < 0) {
+      player.position = 0;
+    }
+    else if (player.position + constants.PADDLE_HEIGHT > constants.GAME_HEIGHT) {
+      player.position = constants.GAME_HEIGHT - constants.PADDLE_HEIGHT;
+    }
+  });
+  // Now that we've processed the queue, empty it
+  actionQueue = [];
+}
+
+function sendSnapshot() {
+  players[0].connection.emit("snapshot", {'id': 1, 'position': players[1].position});
+  players[1].connection.emit("snapshot", {'id': 0, 'position': players[0].position});
+}
+
 function runGame() {
-  
+  initGame();
+  setInterval(computeServerTick, constants.SERVER_TICK_RATE);
+  setInterval(sendSnapshot, constants.SERVER_SNAPSHOT_RATE);
 }
